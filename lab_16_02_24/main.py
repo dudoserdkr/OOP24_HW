fa751faf03e21caeb3636bedd4cfa48f0ba184cb
dudoserdkr <ivyur@knu.ua>
2024-02-24 20:29:36 +0200

    Initial commit

--- lab_16_02_24/main.py
+++
@@ -1,5 +0,0 @@
-from Trap import Trap
-
-d = Trap(32, 4, 16, 144)
-
-print(d.trap_square())
--- lect_12.02.24/t_2.py
+++
@@ -1,9 +0,0 @@
-class initDelTest():
-    def __init__(self):
-        print("InitDelTest")
-
-    def __del__(self):
-        print("InitDeltest: __del__")
-
-
-initDel = initDelTest()
