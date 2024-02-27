753166c35ccdd2ae210a45f766df9737f8ca41da
dudoserdkr <ivyur@knu.ua>
2024-02-27 09:05:22 +0200

    Lab 2 has done

--- Lab_2/Triangle.py
+++ Lab_2/Triangle.py
@@ -1,5 +1,6 @@
 from random import randint as rand
 import turtle as t
+import math
 
 
 class Triangle:
@@ -23,6 +24,23 @@ class Triangle:
 
         self.color = f'#{R:02X}{G:02X}{B:02X}'
 
+
+    def rotate_coords(self, x=None):
+        if x is None:
+            x = rand(1, 360)
+        radians = math.radians(x)
+
+        x = self.start[0] * math.cos(radians) - self.start[0] * math.sin(radians)
+        y = self.start[1] * math.sin(radians) + self.start[1] * math.cos(radians)
+
+        x1 = self.coord_1[0] * math.cos(radians) - self.coord_1[1] * math.sin(radians)
+        y1 = self.coord_1[0] * math.sin(radians) + self.coord_1[1] * math.cos(radians)
+        x2 = self.coord_2[0] * math.cos(radians) - self.coord_2[1] * math.sin(radians)
+        y2 = self.coord_2[0] * math.sin(radians) + self.coord_2[1] * math.cos(radians)
+
+        self.start = (x, y)
+        self.coord_1 = (x1, y1)
+        self.coord_2 = (x2, y2)
 
     def random_pos(self):
         x = rand(-500, 500)
--- Lab_2/main.py
+++ Lab_2/main.py
@@ -3,12 +3,12 @@ import turtle as t
 
 
 triangle = Triangle(100, 100, 100, 0)
-t.speed(0)
+triangle.draw()
 
+# поворот трикутника + 100 рандом трикутниківя
 
 for i in range(100):
-    triangle.random_col()
     triangle.random_pos()
+    triangle.rotate_coords()
+    triangle.random_col()
     triangle.draw()
-
-t.mainloop()
