a33a3d9d58ea01f910da538dce7b25dc32c69f22
dudoserdkr <ivyur@knu.ua>
2024-03-03 17:27:40 +0200

    Triangle has icapsulated

--- Lab_2/Triangle.py
+++ Lab_2/Triangle.py
@@ -4,64 +4,93 @@ import math
 
 
 class Triangle:
+    default_color = '#000000'
 
-    color = '#000000'
-
-    def __init__(self, x1, y1, x2, y2, color=None):
-        self.coord_1 = (x1, y1)
-        self.coord_2 = (x2, y2)
-        if color is not None:
-            self.color = color
-
+    def __init__(self, x1, y1, x2, y2):
+        self._coord_1 = (x1, y1)
+        self._coord_2 = (x2, y2)
+        self._color = Triangle.default_color
         self.start = (0, 0)
 
+    # region setters
+    def set_coord1(self, x1, y1):
+        self._coord_1 = x1, y1
 
-    def random_col(self):
+    def set_coord1(self, x2, y2):
+        self._coord_2 = x2, y2
 
+    def set_color(self, color):
+        # assert is_color_exist(color): -- тут повинна бути перевірка на манкі тест, але її ще нема :(
+        self.color = color
+
+    def set_start(self, x, y):
+        self.start = (x, y)
+
+    def set_random_color(self):
         R = rand(0, 255)
         G = rand(0, 255)
         B = rand(0, 255)
 
-        self.color = f'#{R:02X}{G:02X}{B:02X}'
+        self._color = f'#{R:02X}{G:02X}{B:02X}'
 
-
-    def rotate_coords(self, x=None):
-        if x is None:
-            x = rand(1, 360)
-        
-        radians = math.radians(x)
-        x1 = self.coord_1[0] * math.cos(radians) - self.coord_1[1] * math.sin(radians)
-        y1 = self.coord_1[0] * math.sin(radians) + self.coord_1[1] * math.cos(radians)
-        x2 = self.coord_2[0] * math.cos(radians) - self.coord_2[1] * math.sin(radians)
-        y2 = self.coord_2[0] * math.sin(radians) + self.coord_2[1] * math.cos(radians)
-
-        self.coord_1 = (x1, y1)
-        self.coord_2 = (x2, y2)
-
-    def random_pos(self):
+    def set_random_pos(self):
         x = rand(-500, 500)
         y = rand(-500, 500)
 
         self.start = (x, y)
 
+    # endregion
 
-    def start_pos(self, x=0, y=0):
-        self.start = (x, y)
+    # region getters
+    def get_coord_1(self):
+        return self._coord_1
 
+    def get_coord_2(self):
+        return self._coord_2
 
-    def calc_start_pos(self):
-        v1 = (self.coord_1[0] + self.start[0],
-                        self.coord_1[1] + self.start[1])
+    def get_color(self):
+        return self.color
 
-        v2 = (self.coord_2[0] + self.start[0],
-                        self.coord_2[1] + self.start[1])
+    def get_start(self):
+        return self.start
+
+    # endregion
+
+    # region main functions
+
+    def _random_angle(self):
+        return rand(1, 360)
+
+    def _radians(self, x):
+        if x is None:
+            x = self._random_angle()
+
+        return math.radians(x)
+
+    def _calculate_rotate(self, fi):
+        x1 = self._coord_1[0] * math.cos(fi) - self._coord_1[1] * math.sin(fi)
+        y1 = self._coord_1[0] * math.sin(fi) + self._coord_1[1] * math.cos(fi)
+        x2 = self._coord_2[0] * math.cos(fi) - self._coord_2[1] * math.sin(fi)
+        y2 = self._coord_2[0] * math.sin(fi) + self._coord_2[1] * math.cos(fi)
+
+        return (x1, y1), (x2, y2)
+
+    def rotate_coords(self, x=None):
+        fi = self._radians(x)
+        self._coord_1, self._coord_2 = self._calculate_rotate(fi)
+
+    def _calc_start_pos(self):
+        v1 = (self._coord_1[0] + self.start[0],
+              self._coord_1[1] + self.start[1])
+
+        v2 = (self._coord_2[0] + self.start[0],
+              self._coord_2[1] + self.start[1])
 
         return v1, v2
 
-
     def draw(self):
-        v1, v2 = self.calc_start_pos()
-        t.color(self.color)
+        v1, v2 = self._calc_start_pos()
+        t.color(self._color)
         t.up()
         t.setpos(*self.start)
         t.down()
@@ -69,11 +98,4 @@ class Triangle:
         t.goto(*v2)
         t.setpos(*self.start)
         t.up()
-
-
-if __name__ == "__main__":
-    triangle = Triangle(100, 100, 100, 0)
-    triangle.draw()
-    t.clear()
-
-
+    # endregion
--- Lab_2/main.py
+++ Lab_2/main.py
@@ -1,7 +1,6 @@
 from Triangle import Triangle
 import turtle as t
 
-
 triangle = Triangle(100, 100, 100, 0)
 triangle.draw()
 
@@ -9,15 +8,15 @@ triangle.draw()
 
 for i in range(0, 360):
     triangle.rotate_coords(i)
-    triangle.random_col()
+    triangle.set_random_color()
     triangle.draw()
 
-#100 рандом трикутниківя
+# 100 рандом трикутниківя
 
 for i in range(100):
-    triangle.random_pos()
+    triangle.set_random_pos()
     triangle.rotate_coords()
-    triangle.random_col()
+    triangle.set_random_color()
     triangle.draw()
 
 t.mainloop()
