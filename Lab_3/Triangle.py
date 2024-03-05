b26ed158854d32566cf717515cd14712ee40c03a
dudoserdkr <ivyur@knu.ua>
2024-03-05 19:10:59 +0200

    Scale for triangle has done

--- Lab_3/Triangle.py
+++ Lab_3/Triangle.py
@@ -12,12 +12,14 @@ class Triangle:
             self._coord_2 = x1._coord_2
             self._color = x1._color
             self._start = x1._start
+            self.pivot = x1.pivot
 
         else:
             self._coord_1 = (x1, y1)
             self._coord_2 = (x2, y2)
             self._color = Triangle.default_color
             self._start = (0, 0)
+            self.pivot = self.get_bisec_centre()
 
     # region setters
     def set_coord1(self, x1, y1):
@@ -25,6 +27,9 @@ class Triangle:
 
     def set_coord2(self, x2, y2):
         self._coord_2 = x2, y2
+
+    def set_pivot(self, pivot_x, pivot_y):
+        self.pivot = (pivot_x, pivot_y)
 
     def set_color(self, color):
         assert self._is_color_exist(color)
@@ -63,11 +68,43 @@ class Triangle:
     def get_start(self):
         return self._start
 
+    def get_bisec_centre(self):
+        AB, CB, AC = self.get_side_lenghts()
+
+        ABC = AB + CB + AC
+
+        x_b = (CB * self._start[0] + AC * self._coord_1[0] + AB * self._coord_2[0]) / ABC
+        y_b = (CB * self._start[1] + AC * self._coord_1[1] + AB * self._coord_2[1]) / ABC
+
+        return x_b, y_b
+
+    def get_median_centre(self):
+        x_m = (self._start[0] + self._coord_1[0] + self._coord_2[0]) / 3
+        y_m = (self._start[1] + self._coord_1[1] + self._coord_2[1]) / 3
+
+        return x_m, y_m
+
+    def get_side_lenghts(self):
+
+        AB = ((self._coord_1[0] - self._start[0]) ** 2 + (self._coord_1[1] - self._start[1]) ** 2) ** 0.5
+        CB = ((self._coord_1[0] - self._coord_2[0]) ** 2 + (self._coord_1[1] - self._coord_2[1]) ** 2) ** 0.5
+        AC = ((self._coord_2[0] - self._start[0]) ** 2 + (self._coord_2[1] - self._start[1]) ** 2) ** 0.5
+
+        return AB, CB, AC
+
     # endregion
 
     # region main functions
 
-    def
+    def _calc_scale(self, k):
+
+        x_p, y_p = self.pivot
+
+        for x, y in (self._start, self._coord_1, self._coord_2):
+            new_x = x_p + k * (x - self.pivot[0])
+            new_y = y_p + k * (y - self.pivot[1])
+
+            yield new_x, new_y
 
     @staticmethod
     def _random_angle():
@@ -96,14 +133,13 @@ class Triangle:
         return math.radians(x)
 
     @staticmethod
-    def _calculate_rotate(fi, coord: tuple) -> tuple:
+    def _calculate_rotate(fi, x, y) -> tuple:
         """
         :param fi: кут, на який будуть повертатися координати
-        :param coord: кортеж (x, y)
+        :param x, y: координати точки
         :return: повернуті координати, через матрицю повороту
         """
 
-        x, y = coord
         x1 = x * math.cos(fi) - y * math.sin(fi)
         y1 = x * math.sin(fi) + y * math.cos(fi)
 
@@ -122,24 +158,28 @@ class Triangle:
 
         return v1, v2
 
-    def _calc_dot_rotate(self, x, y, fi):
-        """
-        :param x: координати точки, навколо якої трикутник будет повертатись
-        :param y: координати точки, навколо якої трикутник будет повертатись
+    def _calc_dot_rotate(self, fi):
+        """""
         :param fi: кут, на який трикутник буде повертатись
         :return: повертає 3 кортежа, які містять координати з поворотом
         """
+        x, y = self.pivot
 
         fi = self._radians(fi)
+
         for x1, y1 in (self._start, self._coord_1, self._coord_2):
             cur_x, cur_y = x1 - x, y1 - y
-            new_x, new_y = self._calculate_rotate(fi, (cur_x, cur_y))
+
+            new_x, new_y = self._calculate_rotate(fi, cur_x, cur_y)
 
             yield new_x + x, new_y + y
 
-    def rotate(self, x, y, fi):
-        """Зміняє основні координати трикутника на повернуть"""
-        self._start, self._coord_1, self._coord_2 = self._calc_dot_rotate(x, y, fi)
+    def scale(self, k):
+        self._start, self._coord_1, self._coord_2 = self._calc_scale(k)
+
+    def rotate(self, fi):
+        """Зміняє основні координати трикутника на повернуті"""
+        self._start, self._coord_1, self._coord_2 = self._calc_dot_rotate(fi)
 
     def draw(self):
         t.color(self._color)
@@ -150,4 +190,5 @@ class Triangle:
         t.goto(*self._coord_2)
         t.setpos(*self._start)
         t.up()
+
     # endregion
