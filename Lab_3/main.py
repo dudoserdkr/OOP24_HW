e4b7cd682f93da2139d561fa65ab35589b894db9
dudoserdkr <ivyur@knu.ua>
2024-03-05 19:19:37 +0200

    main good_conditions

--- Lab_3/main.py
+++ Lab_3/main.py
@@ -3,10 +3,23 @@ from turtle import *
 
 speed(0)
 t = Triangle(100, 100, 100, 0)
-t.set_start(1, 1)
+t.set_random_color()
 t.draw()
-t.scale(2)
-t.draw()
-t.rotate(90)
-t.draw()
+
+t.set_pivot(*t.get_bisec_centre())
+
+for i in range(1, 360, 10):
+    t.set_random_color()
+    t.rotate(i)
+    t.draw()
+reset()
+
+t.set_pivot(*t.get_median_centre())
+i = 1
+while i < 3:
+    i += 0.05
+    t.set_random_color()
+    t.scale(i)
+    t.draw()
+
 mainloop()
